ad780ce5ff04a0ea909c1303398aaaf6a30000e4
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2020-07-22 17:48:23 +0530

    Updates

---
+++ precache-manifest.af76773eadf9fc4c3f912413fec2552b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6399565dad686c1729248dfb7b200f3",
+    "url": "/To_Do_App/index.html"
+  },
+  {
+    "revision": "a2249963a6bc428c79d6",
+    "url": "/To_Do_App/static/css/main.0db28e07.chunk.css"
+  },
+  {
+    "revision": "cae303a678086002c6b6",
+    "url": "/To_Do_App/static/js/2.4109f84e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To_Do_App/static/js/2.4109f84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2249963a6bc428c79d6",
+    "url": "/To_Do_App/static/js/main.34c65351.chunk.js"
+  },
+  {
+    "revision": "fb633d6092ac5118e829",
+    "url": "/To_Do_App/static/js/runtime-main.8be23a9b.js"
+  }
+]);
